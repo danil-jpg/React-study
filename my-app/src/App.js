2ec10226f6dd02b39f06161321b1607fc482e170
Danil P <danil.polovinkin.1994@gmail.com>
2023-04-11 11:05:07 +0300

    completed postForm section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,20 @@
 import { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import CssTest from "./components/CssTest";
+import PostList from "./components/PostList";
+import "./styles/App.css";
 
 function App() {
-  let [value, setValue] = useState("str");
+  const [articles, setArticle] = useState([
+    { title: "Title1", btnText: "BtnText1" },
+    { title: "Title2", btnText: "BtnText2" },
+    { title: "Title3", btnText: "BtnText3" },
+  ]);
 
   return (
     <div className="app">
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}></input>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
-      <ClassCounter></ClassCounter>
+      <PostList articles={articles}></PostList>
     </div>
   );
 }
